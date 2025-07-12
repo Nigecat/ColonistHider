abee58290368cfa24e44b5e9426ae1f1454ae157
Nigecat <development@nigelq.com>
2025-07-12 11:12:46 +1000

    Shift source folder

--- Source/Main.cs
+++ Source/Main.cs
@@ -1,15 +1,12 @@
 ﻿using Verse;
-using HarmonyLib;
 
 namespace ColonistHider
 {
     public class Mod : Verse.Mod
     {
-        public override string SettingsCategory() => "Colonist Hider";
-
         public Mod(ModContentPack content) : base(content)
         {
-            new Harmony("Nigecat.ColonistHider").PatchAll();
+       
         }
     }
 }
